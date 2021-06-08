35facb86737fafefc9db51d41a8005865252d0bd
Ruan Rodrigues <rrodri@outlook.com.br>
2021-06-08 17:49:36 -0300

    pipeline

---
+++ Example.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace Example.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var a = 0;
+            var b = 0;
+
+            Assert.Equal(a, b);
+        }
+    }
+}
